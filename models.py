8f38626c5f7239025f0fec0645f1bbc2bb6c63ca
Py.Verse <146224255+PyVerse47@users.noreply.github.com>
2024-02-09 05:32:14 +0330

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ToDo(models.Model):
-    title = models.CharField(max_length=350)
-    complete = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
